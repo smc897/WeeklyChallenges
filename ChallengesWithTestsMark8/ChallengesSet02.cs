b367c9db93ae78e10d1a98e027c0851268de3169
ScottMC89 <scottmc592@gmail.com>
2023-08-18 09:39:20 -0500

    week 2 tests are passing.

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,58 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet02
     {
+        //is c a letter?
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            if (Char.IsLetter(c)) return true;
+            else return false;
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 1) return false;
+            else return true;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 1) return false;
+            else return true;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 == 1) return true;
+            else return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            numbers.OrderBy(num=>num);
+            return numbers.ElementAt(0) + numbers.ElementAt(numbers.Count() - 1);
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length) return str1.Length;
+            else return str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            for (int index = 0; index < numbers.Length; index++) sum += numbers[index];
+            return sum;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            for (int index = 0; index < numbers.Length; index+=2) sum += numbers[index];
+            return sum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            for (int index = 0; index < numbers.Count; index++) sum += numbers[index];
+            if (sum % 2 == 1) return true;
+            else return false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            long count = 0;
+            for (int index = 1; index < number; index++) count++;
+            return count;
         }
     }
 }
